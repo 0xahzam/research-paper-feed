25c41bb8a7eea0739eb84ed9931bb0312a6c3773
Ahzam Khan <104062587+0xahzam@users.noreply.github.com>
2023-01-25 23:45:31 +0530

    request bug

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,88 @@ import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import Fetch from "@/component/fetch";
+import Head from "next/head";
 
 export default function news() {
   return (
-    <Flex>
-      <Tabs>
-        <Flex flexDir={"row"} marginTop={"40px"} gap={{base:"80px"}}>
-          <Flex width={{base:"250px",lg:"70vw"}} marginLeft={"10px"}>
-            <TabPanels>
-              <TabPanel>
-                <Fetch category="physics" />
-              </TabPanel>
-              <TabPanel>
-                <Fetch category="artifical intelligence" />
-              </TabPanel>
-              <TabPanel>
-                <Fetch category="bioinformatics" />
-              </TabPanel>
-            </TabPanels>
-          </Flex>
+    <>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <Flex>
+        <Tabs>
+          <Flex flexDir={"row"} marginTop={"40px"} gap={{ base: "80px" }}>
+            <Flex width={{ base: "250px", lg: "70vw" }} marginLeft={"10px"}>
+              <TabPanels>
+                <TabPanel>
+                  <Fetch category="physics" />
+                </TabPanel>
+                <TabPanel>
+                  <Fetch category="artifical intelligence" />
+                </TabPanel>
+                <TabPanel>
+                  <Fetch category="bioinformatics" />
+                </TabPanel>
+              </TabPanels>
+            </Flex>
 
-          <Flex width={{base:"50px",lg:"20vw"}}>
-            <TabList border={"0px"}>
-              <Flex flexDir={"column"} gap={"4px"}>
-                <Tab
-                  _selected={{color:"blue", background:""}}
-                  _active={""}
-                  m={"0"}
-                  p={"0px 0px 0px 0px"}
-                  display={"flex"}
-                  justifyContent={"right"}
-                >
-                  <Text textAlign={"right"} fontSize={{base:"14px",lg:"18px"}}>physics</Text>
-                </Tab>
-                <Tab
-                  _selected={{color:"blue", background:""}}
-                  _active={""}
-                  m={"0"}
-                  p={"0px 0px 0px 0px"}
-                  display={"flex"}
-                  justifyContent={"right"}
-                >
-                  <Text textAlign={"right"} fontSize={{base:"14px",lg:"18px"}}>ai/ml</Text>
-                </Tab>
-                <Tab
-                  _selected={{color:"blue", background:""}}
-                  _active={""}
-                  m={"0"}
-                  p={"0px 0px 0px 0px"}
-                  display={"flex"}
-                  justifyContent={"right"}
-                >
-                  <Text textAlign={"right"} fontSize={{base:"14px",lg:"18px"}}>bio-info</Text>
-                </Tab>
-              </Flex>
-            </TabList>
+            <Flex width={{ base: "50px", lg: "20vw" }}>
+              <TabList border={"0px"}>
+                <Flex flexDir={"column"} gap={"4px"}>
+                  <Tab
+                    _selected={{ color: "blue", background: "" }}
+                    _active={""}
+                    m={"0"}
+                    p={"0px 0px 0px 0px"}
+                    display={"flex"}
+                    justifyContent={"right"}
+                  >
+                    <Text
+                      textAlign={"right"}
+                      fontSize={{ base: "14px", lg: "18px" }}
+                    >
+                      physics
+                    </Text>
+                  </Tab>
+                  <Tab
+                    _selected={{ color: "blue", background: "" }}
+                    _active={""}
+                    m={"0"}
+                    p={"0px 0px 0px 0px"}
+                    display={"flex"}
+                    justifyContent={"right"}
+                  >
+                    <Text
+                      textAlign={"right"}
+                      fontSize={{ base: "14px", lg: "18px" }}
+                    >
+                      ai/ml
+                    </Text>
+                  </Tab>
+                  <Tab
+                    _selected={{ color: "blue", background: "" }}
+                    _active={""}
+                    m={"0"}
+                    p={"0px 0px 0px 0px"}
+                    display={"flex"}
+                    justifyContent={"right"}
+                  >
+                    <Text
+                      textAlign={"right"}
+                      fontSize={{ base: "14px", lg: "18px" }}
+                    >
+                      bio-info
+                    </Text>
+                  </Tab>
+                </Flex>
+              </TabList>
+            </Flex>
           </Flex>
-        </Flex>
-      </Tabs>
-    </Flex>
+        </Tabs>
+      </Flex>
+    </>
   );
 }
